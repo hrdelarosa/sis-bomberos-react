30209b7fd5d724610058f33bfe4abb40519cbe07
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-04-06 02:12:12 -0600

    feat: agregar react-hook-form y nuevas rutas para gestión de personal, estaciones y usuarios

--- src/layouts/components/Main.tsx
+++ src/layouts/components/Main.tsx
@@ -6,7 +6,8 @@ interface Props {
 export default function Main({ children, closeUserDropdown }: Props) {
   return (
     <main
-      className="main-layout flex-1 bg-white p-4 pt-0 overflow-y-auto overflow-x-auto"
+      className="main-layout relative flex-1 bg-white p-4 pt-0 overflow-y-auto overflow-x-auto"
+      id="content_main"
       onClick={closeUserDropdown}
     >
       {children}
