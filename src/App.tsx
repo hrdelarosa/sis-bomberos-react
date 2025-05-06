3d4bff28cbe4988246e547f5773af88f64eecdac
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-05-05 23:46:25 -0600

    feat: cambiar el idioma de la aplicación a español y mejorar la estructura de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import AppRoutes from './Routes'
+import AppRoutes from './routes/Routes'
+import { Toaster } from 'sonner'
 import './App.css'
 
 function App() {
-  return <AppRoutes />
+  return (
+    <>
+      <Toaster position="top-right" richColors closeButton visibleToasts={6} />
+      <AppRoutes />
+    </>
+  )
 }
 
 export default App
